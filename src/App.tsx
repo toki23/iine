f5aba443175ebfc8805b359eb6a936ecb00cae3b
Sagi Sorata <m18063@g.metro-cit.ac.jp>
2022-05-17 13:49:09 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App () {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+<h1 className='text-3xl'>HEllo react</h1>
+    </div>
+  )
+}
+
+export default App
