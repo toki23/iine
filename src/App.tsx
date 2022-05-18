1afd0ab5d89ac39eab51f77451984a22d7b450b3
Sagi Sorata <m18063@g.metro-cit.ac.jp>
2022-05-18 11:15:09 +0900

    linter,formatterの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-
-function App () {
-  const [count, setCount] = useState(0)
+function App (): JSX.Element {
   return (
-    <div className="App">
-<h1 className='text-3xl'>HEllo react</h1>
+    <div className='App'>
+      <h1 className='text-3xl'>HEllo react</h1>
     </div>
   )
 }
